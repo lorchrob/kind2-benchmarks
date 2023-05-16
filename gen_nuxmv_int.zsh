51de462429ace887e1b9ad8db6908a0e79ca537a
Rob Lorch <lorchrob@gmail.com>
2023-05-16 18:01:52 -0400

    Update benchmarks

--- gen_nuxmv_int.zsh
+++ gen_nuxmv_int.zsh
@@ -10,50 +10,50 @@ for file in *; do
   echo $file
 done
 
-cd ../memory1
-for file in *; do 
-  echo $file
-  touch "./../../nuxmv_models/memory1/${file%.*}.smv"
-  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
-  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/memory1/${file%.*}.smv"
-  echo $file
-done
+# cd ../memory1
+# for file in *; do 
+#   echo $file
+#   touch "./../../nuxmv_models/memory1/${file%.*}.smv"
+#   timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+#   cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/memory1/${file%.*}.smv"
+#   echo $file
+# done
 
-cd ../memory2
-for file in *; do 
-  echo $file
-  touch "./../../nuxmv_models/memory2/${file%.*}.smv"
-  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
-  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/memory2/${file%.*}.smv"
-  echo $file
-done
+# cd ../memory2
+# for file in *; do 
+#   echo $file
+#   touch "./../../nuxmv_models/memory2/${file%.*}.smv"
+#   timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+#   cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/memory2/${file%.*}.smv"
+#   echo $file
+# done
 
-cd ../misc
-for file in *; do 
-  echo $file
-  touch "./../../nuxmv_models/misc/${file%.*}.smv"
-  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
-  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/misc/${file%.*}.smv"
-  echo $file
-done
+# cd ../misc
+# for file in *; do 
+#   echo $file
+#   touch "./../../nuxmv_models/misc/${file%.*}.smv"
+#   timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+#   cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/misc/${file%.*}.smv"
+#   echo $file
+# done
 
-cd ../protocol
-for file in *; do 
-  echo $file
-  touch "./../../nuxmv_models/protocol/${file%.*}.smv"
-  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
-  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/protocol/${file%.*}.smv"
-  echo $file
-done
+# cd ../protocol
+# for file in *; do 
+#   echo $file
+#   touch "./../../nuxmv_models/protocol/${file%.*}.smv"
+#   timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+#   cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/protocol/${file%.*}.smv"
+#   echo $file
+# done
 
-cd ../simulation
-for file in *; do 
-  echo $file
-  touch "./../../nuxmv_models/simulation/${file%.*}.smv"
-  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
-  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/simulation/${file%.*}.smv"
-  echo $file
-done
+# cd ../simulation
+# for file in *; do 
+#   echo $file
+#   touch "./../../nuxmv_models/simulation/${file%.*}.smv"
+#   timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+#   cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/simulation/${file%.*}.smv"
+#   echo $file
+# done
 
 cd ../Real_Int/large
 for file in *; do 
---
+++ gen_nuxmv_int32.zsh
@@ -0,0 +1,65 @@
+#!/bin/zsh
+source ~/.zshrc
+cd ./old_benchmarks/int32/kind2_models/large
+
+for file in *; do 
+  echo $file
+  touch "./../../nuxmv_models/large/${file%.*}.smv"
+  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/large/${file%.*}.smv"
+  echo $file
+done
+
+cd ../memory1
+for file in *; do 
+  echo $file
+  touch "./../../nuxmv_models/memory1/${file%.*}.smv"
+  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/memory1/${file%.*}.smv"
+  echo $file
+done
+
+cd ../memory2
+for file in *; do 
+  echo $file
+  touch "./../../nuxmv_models/memory2/${file%.*}.smv"
+  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/memory2/${file%.*}.smv"
+  echo $file
+done
+
+cd ../misc
+for file in *; do 
+  echo $file
+  touch "./../../nuxmv_models/misc/${file%.*}.smv"
+  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/misc/${file%.*}.smv"
+  echo $file
+done
+
+cd ../protocol
+for file in *; do 
+  echo $file
+  touch "./../../nuxmv_models/protocol/${file%.*}.smv"
+  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/protocol/${file%.*}.smv"
+  echo $file
+done
+
+cd ../simulation
+for file in *; do 
+  echo $file
+  touch "./../../nuxmv_models/simulation/${file%.*}.smv"
+  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+  cp "/Users/lorchrob/Desktop/out.smv" "./../../nuxmv_models/simulation/${file%.*}.smv"
+  echo $file
+done
+
+cd ../Real_Int32/large
+for file in *; do 
+  echo $file
+  touch "./../../../nuxmv_models/Real_Int32/large/${file%.*}.smv"
+  timeout 1 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' $file 
+  cp "/Users/lorchrob/Desktop/out.smv" "./../../../nuxmv_models/Real_Int32/large/${file%.*}.smv"
+  echo $file
+done
