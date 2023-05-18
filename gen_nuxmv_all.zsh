9aa2c10a3d96190f1e924c2779bb8a5a037d5978
Rob Lorch <lorchrob@gmail.com>
2023-05-18 13:28:12 -0400

    Run updated translator on all nuxmv models

---
+++ gen_nuxmv_all.zsh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+./gen_nuxmv_bool.zsh;
+./gen_nuxmv_functional_chain.zsh;
+./gen_nuxmv_int.zsh;
+./gen_nuxmv_int32.zsh;
+./gen_nuxmv_new.zsh;
+./gen_nuxmv_real_int.zsh;
+./gen_nuxmv_real_int32.zsh
