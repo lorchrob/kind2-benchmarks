0ff9f17c6159047322d00c9d7b47d65c4ea51d77
Rob Lorch <lorchrob@gmail.com>
2023-05-23 13:55:12 -0400

    Split and combine models so that we have all combinations

---
+++ split_props.py
@@ -0,0 +1,21 @@
+import sys
+import re
+
+filename = "kind2_models/TopLevel_v6"
+num_props = 9
+
+for i in range(1, num_props+1):
+  f = open(filename + ".lus", "r")
+  f_new = open(filename + "__" + str(i) + ".lus", "w")
+  j = 0
+  for line in f:
+    if "--%PROPERTY" in line.strip():
+      j += 1 
+      if j == i:
+        f_new.write(line) 
+      else:
+        continue
+    else:
+      f_new.write(line)
+    
+  
