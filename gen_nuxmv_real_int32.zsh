978ee6228956bcf95cf4fc33582c1c42abd3a8f6
Rob Lorch <lorchrob@gmail.com>
2023-05-25 18:43:59 -0400

    Remove slicing

--- gen_nuxmv_real_int32.zsh
+++ gen_nuxmv_real_int32.zsh
@@ -5,7 +5,7 @@ cd ./old_benchmarks/real_int32/kind2_models
 for file in *; do 
   echo $file
   touch "./../nuxmv_models/${file%.*}.smv"
-  timeout .5 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' '--slice_nodes' 'false' $file 
+  timeout .25 '/Users/lorchrob/Documents/CodeProjects/kind2_parent/kind2/bin/kind2' '--slice_nodes' 'false' $file 
   cp "/Users/lorchrob/Desktop/out.smv" "./../nuxmv_models/${file%.*}.smv"
   echo $file
 done
