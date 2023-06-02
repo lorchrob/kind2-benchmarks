3d10caec81301d7d9c879bf8b10ad64dd587220e
Rob Lorch <lorchrob@gmail.com>
2023-06-01 20:12:00 -0400

    Tweaks

--- scripts/classify_models.py
+++ scripts/classify_models.py
@@ -4,7 +4,7 @@ import os
 ##!! Maybe copy lustre models to both Kind2 and JKind, then manually update
 
 # Loop through files
-for subdir, dirs, files in os.walk("/Users/lorchrob/Desktop/kind2-benchmarks/FMCAD23"):
+for subdir, dirs, files in os.walk("/Users/lorchrob/Desktop/kind2-benchmarks/kind2-benchmarks"):
   for filename in files:
     if ".lus" in filename:
       model_type = "lustre"
@@ -12,6 +12,11 @@ for subdir, dirs, files in os.walk("/Users/lorchrob/Desktop/kind2-benchmarks/FMC
       model_type = "smv"
     else:
       continue
+
+    if model_type == "lustre":
+      model_dir = "kind2_models"
+    else:
+      model_dir = "nuxmv_models"
     
     num_props = 0
     has_array = False
@@ -22,15 +27,23 @@ for subdir, dirs, files in os.walk("/Users/lorchrob/Desktop/kind2-benchmarks/FMC
     has_int = False 
     
     f = open(os.path.join(subdir, filename), "r")
+
+
     
     for line in f:
       # Count properties and record types
-      if "--%PROPERTY" in line:
+      if "--%PROPERTY" in line or "INVARSPEC" in line:
         num_props += 1 
-      if "array" in line or "^" in line or "[10]" in line or "[4]" in line: 
+      if "array" in line or "^" in line:
+        has_array = True
+      elif "[10]" in line and model_dir == "kind2_models": 
         has_array = True 
-      if "function " in line or "FUN" in line: 
+        model_dir = "jkind_models"
+      if "function imported" in line or "FUN" in line: 
         has_uf = True
+      elif "function C" in line and model_dir == "kind2_models":
+        has_uf = True 
+        model_dir = "jkind_models"
       if ":real" in line or ": real" in line or "real;" in line:
         has_real = True 
       if "int32" in line or "signed word[32]" in line: 
@@ -45,27 +58,26 @@ for subdir, dirs, files in os.walk("/Users/lorchrob/Desktop/kind2-benchmarks/FMC
     else: 
       prop_dir = "multi-prop"
       
-    if model_type == "lustre":
-      model_dir = "kind2_models"
-    else:
-      model_dir = "nuxmv_models"
     
     # Send file to correct location
     if has_array:
-      out_dir = "/Users/lorchrob/Desktop/kind2-benchmarks/FMCAD23/{model_dir}/{prop_dir}/array/{filename}"
-    if has_bool and not (has_real or has_int32 or has_int): 
-      out_dir = "/Users/lorchrob/Desktop/kind2-benchmarks/FMCAD23/{model_dir}/{prop_dir}/bool/{filename}"
-    if has_int and not (has_real or has_int32):
-      out_dir = "/Users/lorchrob/Desktop/kind2-benchmarks/FMCAD23/{model_dir}/{prop_dir}/int/{filename}"
-    if has_real and has_int and not has_int32:
-      out_dir = "/Users/lorchrob/Desktop/kind2-benchmarks/FMCAD23/{model_dir}/{prop_dir}/int-real/{filename}" 
-    if has_int32 and not has_real:
-      out_dir = "/Users/lorchrob/Desktop/kind2-benchmarks/FMCAD23/{model_dir}/{prop_dir}/int32/{filename}" 
-    if has_real and not (has_int32 or has_int):
-      out_dir = "/Users/lorchrob/Desktop/kind2-benchmarks/FMCAD23/{model_dir}/{prop_dir}/real/{filename}" 
-    if has_uf:
-      out_dir = "/Users/lorchrob/Desktop/kind2-benchmarks/FMCAD23/{model_dir}/{prop_dir}/uf/{filename}"
+      out_dir = f"/Users/lorchrob/Desktop/kind2-benchmarks/kind2-benchmarks/{model_dir}/{prop_dir}/array/{filename}"
+    elif has_uf:
+      out_dir = f"/Users/lorchrob/Desktop/kind2-benchmarks/kind2-benchmarks/{model_dir}/{prop_dir}/uf/{filename}"
+    elif has_bool and not (has_real or has_int32 or has_int): 
+      out_dir = f"/Users/lorchrob/Desktop/kind2-benchmarks/kind2-benchmarks/{model_dir}/{prop_dir}/bool/{filename}"
+    elif has_int and not (has_real or has_int32):
+      out_dir = f"/Users/lorchrob/Desktop/kind2-benchmarks/kind2-benchmarks/{model_dir}/{prop_dir}/int/{filename}"
+    elif has_real and has_int and not has_int32:
+      out_dir = f"/Users/lorchrob/Desktop/kind2-benchmarks/kind2-benchmarks/{model_dir}/{prop_dir}/int-real/{filename}" 
+    elif has_int32 and not has_real:
+      out_dir = f"/Users/lorchrob/Desktop/kind2-benchmarks/kind2-benchmarks/{model_dir}/{prop_dir}/int32/{filename}" 
+    elif has_real and not (has_int32 or has_int):
+      out_dir = f"/Users/lorchrob/Desktop/kind2-benchmarks/kind2-benchmarks/{model_dir}/{prop_dir}/real/{filename}" 
+    else:
+      out_dir = os.path.join(subdir, filename)
+      print("FAILED:", out_dir)
     
-    
-    os.replace(os.path.join(subdir, filename), out_dir)
+    print(os.path.join(subdir, filename), "TO", out_dir)
+    os.rename(os.path.join(subdir, filename), out_dir)
       
